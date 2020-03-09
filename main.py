0db2ead4586b9ae2a40e004b09b526406ddb15ae
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-03-09 14:34:46 -0400

    u

--- main.py
+++ main.py
@@ -57,6 +57,39 @@ info = {
 	"url": "https://swapi.co/api/people/1/"
 }
 
+planet = {"name": "Tatooine", 
+    "rotation_period": "23", 
+    "orbital_period": "304", 
+    "diameter": "10465", 
+    "climate": "arid", 
+    "gravity": "1 standard", 
+    "terrain": "desert", 
+    "surface_water": "1", 
+    "population": "200000", 
+    "residents": [
+        "https://swapi.co/api/people/1/", 
+        "https://swapi.co/api/people/2/", 
+        "https://swapi.co/api/people/4/", 
+        "https://swapi.co/api/people/6/", 
+        "https://swapi.co/api/people/7/", 
+        "https://swapi.co/api/people/8/", 
+        "https://swapi.co/api/people/9/", 
+        "https://swapi.co/api/people/11/", 
+        "https://swapi.co/api/people/43/", 
+        "https://swapi.co/api/people/62/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/5/", 
+        "https://swapi.co/api/films/4/", 
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/3/", 
+        "https://swapi.co/api/films/1/"
+    ], 
+    "created": "2014-12-09T13:50:49.641000Z", 
+    "edited": "2014-12-21T20:48:04.175778Z", 
+    "url": "https://swapi.co/api/planets/1/"
+}
+
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
 
@@ -83,6 +116,10 @@ else:
 print("")
 input("Since we're on the topic of space,")
 response = input("have you ever heard of " + info["name"] + "?  ")
-if response == "Yes" :
-    star = input(" Wait, wait... are you a Star Wars geek too?  ")
-    if star == "Yes"
+if response == "No" :
+	print("Well he's from Star Wars, maybe you've heard of that.")
+else:
+	print("Well then I hope you're as big of a geek as I am.")
+
+print("")
+input("Anyways, I wanted to ")
